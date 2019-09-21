342afe8c518a9ede25c056658d5bec41efdd5085
Stephane <apsarawebdev@protonmail.com>
2019-09-21 17:39:01 +0800

    vanilla pour before load

--- js/beforeLoad.js
+++ js/beforeLoad.js
@@ -1,11 +1,14 @@
 window.onbeforeunload = function(event) {
 
+	var el1 = document.getElementById('main').style;
+	var el2 = document.getElementById('main-2').style;
 
+	el1.display = 'block';
+	el1.position = 'relative';
+	el1.top = '0px';
+	el1.transition = '0s';
+	el2.display = 'none';
 
-    $('#main').css('display', 'block').animate({'position': 'relative', 'top':'0px'}, 0);
-
-
-	$('#main-2').css({'display': 'none'});
-	// $('body, html').css('overflow', 'hidden');
+	console.log(el1.transition = '0s');
 
 };
